c0033dbd7fa6d9f5f1f64a8420400e4a7e52172f
HuzoDumpster <brawlzteamm@gmail.com>
2025-11-04 13:38:27 +0100

    Muokattu tulostusviesti main-haarassa v1

--- main.go
+++ main.go
@@ -53,6 +53,7 @@ func sha256file(filename string) (string, error) {
 
 
 func main() {
+    fmt.Println("Checksum application v1")
     // Define Flags & Help Messages
     var fileFlag   = flag.String("file", "", "Path to a single file.");
     var folderFlag = flag.String("folder", "", "Path to a folder with files to get the checksum of.");
